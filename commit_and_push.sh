c48fe41eb9f01ebf1099dd69d02555d91ec1f953
VictorLAUREA <victor.morisson@laurea.fr>
2024-06-21 18:04:01 +0200

    MAJ Nom fichiers PNG

--- commit_and_push.sh
+++ commit_and_push.sh
@@ -4,8 +4,8 @@
 dossier="C:\Users\vmorisson\OneDrive - PLENITUDE\Documents\LOGISTIQUE\STOCK\Gestion Stock\INFO-LOG"
 
 # Ajouter les fichiers à l'index
-git add "$dossier/TCD 1.PNG"
-git add "$dossier/TCD 2.PNG"
+git add "$dossier/TCD Stock.PNG"
+git add "$dossier/Suivi Dossiers.PNG"
 git add "$dossier/index.html"
 
 # Commit avec le message "MAJ" suivi de la date et du numéro de version par journée
