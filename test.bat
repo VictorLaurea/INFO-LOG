d9adcb2e7c64b3b9797891274f8d4e1437d68a99
VictorLAUREA <victor.morisson@laurea.fr>
2024-08-22 17:29:18 +0200

    Bat

--- test.bat
+++ test.bat
@@ -1,15 +1,6 @@
-@echo off
-REM Naviguer vers le répertoire de travail
-cd "\\laurea-files\Datas\Technique\STOCK\INFO-LOG"
-
-REM Ajouter tous les fichiers modifiés à l'index Git
-git add .
-
-REM Effectuer un commit avec le message "MAJ"
+cd /d P:\Technique\STOCK\INFO-LOG
+git add Index.html
+git add TCDStock.PNG
+git add SuiviDossiers.PNG
 git commit -m "MAJ"
-
-REM Pousser les modifications vers la branche 'main' du dépôt distant
 git push origin main
-
-REM Temporisation pour maintenir la fenêtre ouverte
-pause
