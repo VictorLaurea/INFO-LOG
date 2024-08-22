982c3ed6195211872824bf9cad2010a2e70ea801
VictorLAUREA <victor.morisson@laurea.fr>
2024-08-22 13:13:50 +0200

    MAJ ignore et commit

--- commit_and_push.sh
+++ commit_and_push.sh
@@ -3,10 +3,13 @@
 # Chemin vers votre dossier où se trouvent les fichiers
 dossier="C:\Users\vmorisson\OneDrive - PLENITUDE\Documents\LOGISTIQUE\STOCK\Gestion Stock\INFO-LOG"
 
+
+
 # Ajouter les fichiers à l'index
 git add "$dossier/TCDStock.PNG"
 git add "$dossier/SuiviDossiers.PNG"
 git add "$dossier/index.html"
+git add "$dossier/.gitignore.txt"
 
 # Commit avec le message "MAJ" suivi de la date et du numéro de version par journée
 date_version=$(date +'%Y-%m-%d-%H-%M-%S') # Format de date personnalisé
@@ -14,3 +17,7 @@ git commit -m "MAJ du $date_version"
 
 # Pousser les modifications vers le dépôt distant
 git push origin main
+
+#Temporisation
+echo "Appuyez sur une touche pour continuer..."
+read -n 1
