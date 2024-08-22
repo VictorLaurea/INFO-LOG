68ee49752d1f69b0893d9a9aeadb7123423e3071
VictorLAUREA <victor.morisson@laurea.fr>
2024-08-22 14:17:25 +0200

    Test nouveau git

---
+++ commit_and_push.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Chemin vers votre dossier où se trouvent les fichiers
+dossier="P:\Technique\STOCK\INFO-LOG"
+
+# Ajouter les fichiers à l'index
+git add "$dossier/TCDStock.PNG"
+git add "$dossier/SuiviDossiers.PNG"
+git add "$dossier/index.html"
+git add "$dossier/.gitignore"
+
+# Commit avec le message "MAJ" suivi de la date et du numéro de version par journée
+date_version=$(date +'%Y-%m-%d-%H-%M-%S') # Format de date personnalisé
+git commit -m "MAJ du $date_version"
+
+# Pousser les modifications vers le dépôt distant
+git push origin main
+
+# Temporisation
+echo "Appuyez sur une touche pour continuer..."
+read -n 1
