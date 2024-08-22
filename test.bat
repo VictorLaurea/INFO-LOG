376fbd6f30ceb5dbd7d0ce4a32d757c703bc6edf
VictorLAUREA <victor.morisson@laurea.fr>
2024-08-22 15:44:54 +0200

    MAJ

---
+++ test.bat
@@ -0,0 +1,15 @@
+@echo off
+REM Naviguer vers le répertoire de travail
+cd /d "\\laurea-files\Datas\Technique\STOCK\INFO-LOG"
+
+REM Ajouter tous les fichiers modifiés à l'index Git
+git add .
+
+REM Effectuer un commit avec le message "MAJ"
+git commit -m "MAJ"
+
+REM Pousser les modifications vers la branche 'main' du dépôt distant
+git push origin main
+
+REM Temporisation pour maintenir la fenêtre ouverte
+pause
